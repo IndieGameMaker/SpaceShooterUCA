dd4ea79f31cfd6058482f567904d957bae0a557b
Lee JaeHyun <myleje@gmail.com>
2024-04-15 20:13:57 +0900

    .

--- Assets/02_Scripts/RemoveBullet.cs
+++ Assets/02_Scripts/RemoveBullet.cs
@@ -29,9 +29,25 @@ public class RemoveBullet : MonoBehaviour
 
     void DestroyBullet(Collision coll)
     {
-        Instantiate(_sparkEffect, coll.transform.position, Quaternion.identity);
+        // 충돌 좌표(지점)
+        ContactPoint cp = coll.GetContact(0);
+
+        Vector3 pos = cp.point; // 첫 번째 충돌 좌표
+        Vector3 _normal = -1 * cp.normal; // 충돌한 지점의 법선 벡터
+
+        var rot = Quaternion.LookRotation(_normal);
+
+        var obj = Instantiate(_sparkEffect, pos, rot);
+        Destroy(obj, 0.8f);
+
         Destroy(coll.gameObject);
     }
+
+    /*
+    Quaternion (쿼터니언 : 사원수) x, y, z, w
+    짐벌락 (Gimbal Lock)를 방지하기 위해 사용하는 자료형
+    
+    */
 
 }
 
