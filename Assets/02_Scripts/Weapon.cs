d76e1a4639b8ab68471f939f79b6fa544e0da160
Lee JaeHyun <myleje@gmail.com>
2024-04-15 19:08:15 +0900

    .

--- Assets/02_Scripts/Weapon.cs
+++ Assets/02_Scripts/Weapon.cs
@@ -8,11 +8,23 @@ public class Weapon : MonoBehaviour
     [SerializeField] private Transform _firePos;
     [SerializeField] private AudioClip _fireSfx;
 
+    private AudioSource _audio;
+
+    void Start()
+    {
+        _audio = GetComponent<AudioSource>();
+    }
+
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Instantiate(_bulletPrefab, _firePos.position, _firePos.rotation);
+            Fire();
         }
+    }
+
+    void Fire()
+    {
+        Instantiate(_bulletPrefab, _firePos.position, _firePos.rotation);
     }
 }
