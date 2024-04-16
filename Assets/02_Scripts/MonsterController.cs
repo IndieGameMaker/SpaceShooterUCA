6460da87ff846a552fb5f668325a2dd96c576403
Lee JaeHyun <myleje@gmail.com>
2024-04-16 21:15:32 +0900

    .

--- Assets/02_Scripts/MonsterController.cs
+++ Assets/02_Scripts/MonsterController.cs
@@ -6,12 +6,16 @@ public class MonsterController : MonoBehaviour
 {
     public enum State { IDLE, TRACE, ATTACK, DIE }
 
+    // 몬스터의 상태를 저장하는 변수
     [SerializeField] private State _state = State.IDLE;
 
     // 추적 사정거리
     [SerializeField] private float _traceDist = 10.0f;
     // 공격 사정거리
     [SerializeField] private float _attackDist = 2.0f;
+
+    private Transform _monsterTr;
+    private Transform _playerTr;
 
     // Start is called before the first frame update
     void Start()
