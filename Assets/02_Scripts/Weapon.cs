669207254c51bf3fceb8a7dc8fed3b8eb916b361
Lee JaeHyun <myleje@gmail.com>
2024-04-16 20:16:22 +0900

    .

--- Assets/02_Scripts/Weapon.cs
+++ Assets/02_Scripts/Weapon.cs
@@ -45,12 +45,17 @@ public class Weapon : MonoBehaviour
         // 오프셋 변경
         // (0, 0.5) (0.5, 0) (0.5, 0.5)
         // Random.Range(0, 2) => (0, 1) * 0.5 => (0, 0.5)
-
         Vector2 _offset = new Vector2(Random.Range(0, 2), Random.Range(0, 2)) * 0.5f;
         _muzzleFlash.material.mainTextureOffset = _offset;
 
+        // Scale
         Vector3 _scale = Vector3.one * Random.Range(1.0f, 3.0f);
         _muzzleFlash.transform.localScale = _scale;
+
+        // 회전처리
+        float angle = Random.Range(0, 360);
+        Quaternion rot = Quaternion.Euler(Vector3.forward * angle);
+        _muzzleFlash.transform.localRotation = rot;
 
         // 블랭크 효과
         _muzzleFlash.enabled = true;
