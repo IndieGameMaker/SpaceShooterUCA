538bd74052d3a890a00f1ec9a2dc2feba7c49ad4
Lee JaeHyun <myleje@gmail.com>
2024-04-16 21:11:43 +0900

    .

--- Assets/02_Scripts/MonsterController.cs
+++ Assets/02_Scripts/MonsterController.cs
@@ -8,6 +8,10 @@ public class MonsterController : MonoBehaviour
 
     [SerializeField] private State _state = State.IDLE;
 
+    // 추적 사정거리
+    [SerializeField] private float _traceDist = 10.0f;
+
+
     // Start is called before the first frame update
     void Start()
     {
