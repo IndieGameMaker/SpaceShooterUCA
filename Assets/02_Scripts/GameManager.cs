c32cdfdac6870038480523ca1e5ac8cf8a067501
Lee JaeHyun <myleje@gmail.com>
2024-04-18 20:40:53 +0900

    .

--- Assets/02_Scripts/GameManager.cs
+++ Assets/02_Scripts/GameManager.cs
@@ -7,6 +7,9 @@ public class GameManager : MonoBehaviour
     public GameObject monsterPrefab;
     public List<Transform> points = new List<Transform>();
 
+    // 몬스터 생성 주기
+    public float createTime = 3.0f;
+
     void Start()
     {
         // SpawnPointGroup GameObject를 검색
@@ -16,6 +19,21 @@ public class GameManager : MonoBehaviour
 
         // Resources 폴더에 있는 Asset(Prefab)을 로드
         monsterPrefab = Resources.Load<GameObject>("Monster");
+
+        // 몬스터 생성
+        // 이벤트명.Invoke();
+        // Invoke("함수명", 지연시간);
+
+        InvokeRepeating("CreateMonster", 2.0f, createTime);
     }
 
+    void CreateMonster()
+    {
+        // 난수 발생
+        int idx = Random.Range(1, points.Count);
+        // 생성할 위치 추출
+        Vector3 pos = points[idx].position;
+        // 몬스터를 생성
+        Instantiate(monsterPrefab, pos, Quaternion.identity);
+    }
 }
