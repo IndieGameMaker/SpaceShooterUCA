a0a3c57fe252314b3020aa7fcd7276a4a698ad28
Lee JaeHyun <myleje@gmail.com>
2024-04-18 20:32:07 +0900

    .

--- Assets/02_Scripts/GameManager.cs
+++ Assets/02_Scripts/GameManager.cs
@@ -13,6 +13,9 @@ public class GameManager : MonoBehaviour
         GameObject group = GameObject.Find("SpawnPointGroup");
         // List 변수에 할당
         group?.GetComponentsInChildren<Transform>(points);
+
+        // Resources 폴더에 있는 Asset(Prefab)을 로드
+        monsterPrefab = Resources.Load<GameObject>("Monster");
     }
 
 }
