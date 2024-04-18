4574fc913ff3743e95d55f27a5909cd364510225
Lee JaeHyun <myleje@gmail.com>
2024-04-18 20:11:39 +0900

    .

---
+++ Assets/02_Scripts/MyGizmos.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyGizmos : MonoBehaviour
+{
+    public Color color = Color.green;
+
+    [Range(0.2f, 2.0f)]
+    public float radius = 0.3f;
+}
