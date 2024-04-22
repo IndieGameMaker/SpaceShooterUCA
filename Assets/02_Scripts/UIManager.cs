3fdf39a0412cf17f753616a8298f87322e27a962
Lee JaeHyun <myleje@gmail.com>
2024-04-22 19:28:45 +0900

    .

---
+++ Assets/02_Scripts/UIManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // 씬 로딩
+using UnityEngine.UI; // UGUI
+using TMPro; // TextMeshPro
+
+// using UnityEngine.AI; // 네비게이션
+
+public class UIManager : MonoBehaviour
+{
+
+    public void OnStartGameButtonClick()
+    {
+        Debug.Log("Click StartGame");
+    }
+}
